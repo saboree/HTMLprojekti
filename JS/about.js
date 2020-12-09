828472ab85160034188c9e0b8d0d5d793ea41379
Sasha Wouse <snowpill@hotmail.fi>
2020-12-09 04:21:32 +0200

    update widget

--- JS/about.js
+++ JS/about.js
@@ -6,3 +6,13 @@ menuBtn.addEventListener('click', function (){
     menuBtn.classList.toggle('active');
     menu.classList.toggle('active');
 });
+
+//Background for weather widget
+theTime = new Date();
+theHour = theTime.getHours();
+if(theHour > 6 && theHour < 18){
+    document.write('<article background="/Images/weatherbg.jpeg" text="day" >');
+}
+else{
+    document.write('<article background="/Images/starsBg.jpg" text="night" >');
+}
